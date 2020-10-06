427c050fc66693cde369dad886374c775341d2b7
jollywatt <joalexw@gmail.com>
2020-10-06 23:34:15 +1300

    (Documentation test 4)

--- docs/make.jl
+++ docs/make.jl
@@ -15,7 +15,7 @@ function make(; kwargs...)
 
 	deploydocs(
 		repo = "github.com/Jollywatt/GeometricAlgebra.jl.git",
-		versions = ["stable" => "v^", "v#.#", devurl => devurl],
+		versions = ["stable" => "v^", "v#.#", "dev" => "dev"],
 	)
 end
 

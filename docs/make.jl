4223aeba8f2a2fcfa54cc73255ef063fe51dcb65
jollywatt <joalexw@gmail.com>
2020-10-06 22:39:10 +1300

    Initial docs
    Setup Documenter.jl to deploy docs via Github Action

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using Pkg, Documenter
+using GeometricAlgebra
+
+# run all doctests in doc strings afer using GeometricAlgebra
+DocMeta.setdocmeta!(GeometricAlgebra, :DocTestSetup, quote
+	using GeometricAlgebra
+end; recursive=true)
+
+function make(; kwargs...)
+	makedocs(
+		root=Pkg.dir("GeometricAlgebra", "docs"),
+		sitename="GeometricAlgebra.jl",
+		modules=[GeometricAlgebra];
+		kwargs...)
+
+	deploydocs(
+	    repo = "github.com/Jollywatt/GeometricAlgebra.jl.git",
+	)
+end
+
+make()

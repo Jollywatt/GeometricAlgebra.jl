40bf1f247784f95d5ff5c93ccec10ac7652f8970
jollywatt <jo.alex.w@gmail.com>
2021-04-05 11:48:52 +1200

    Added a few more basic tests
    (Just want to see how codecov badge improves...)

--- test/arithmetic.jl
+++ test/arithmetic.jl
@@ -1,12 +1,13 @@
 using GeometricAlgebra
 
 @testset "constructors" begin
-
 	@test iszero(zero(Blade{(1,1,1),2,0b101,Float64}))
 	@test iszero(zero(Blade{(1,1,1),0,0b000,BigInt}))
 	@test iszero(zero(Multivector{(1,1,1),1,Vector{Int}}))
 	@test iszero(zero(MixedMultivector{(1,1,1),Vector{Int}}))
 
+	@test first(basis((1,1,1))) == Blade{(1,1,1)}(1, 0b001)
+	@test  last(basis((1,1,1))) == Blade{(1,1,1)}(1, 0b100)
 end
 
 @testset "equality" begin
@@ -60,4 +61,29 @@ end
 	@test (7A)*2 == 2B
 	@test (7 + 0im)A == (1//1)B
 
+end
+
+@testset "addition, subtraction" begin
+	sig = (1, 1, 1)
+	x, y, z = basis(sig)
+	@test x + y == Multivector{sig,1}([1, 1, 0])
+	@test x + y == y + x
+	@test (x + y) + z == y + x + z
+	@test x + 2 == MixedMultivector{sig}([2, 1, 0, 0, 0, 0, 0, 0])
+	@test x + 0 == x
+	@test x + 3.0y == (3//1)y + x
+
+	u = x + 2y
+	@test -x == (-1)x
+	@test -u == (-1)u
+	@test x - y == x + (-y)
+	@test u - 1 == -1 + u
+	@test u - x == u - (0 + x)
+end
+
+@testset "geometric product of blades" begin
+    x, y, z = basis((1, 1, 1))
+    @test x*x == y*y == z*z == 1
+    @test x*y == -y*x
+    @test (x*y)*(x*y) == -1
 end

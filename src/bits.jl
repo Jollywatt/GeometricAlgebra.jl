5e506566bec39bd02c98ff3f5f34e5d4e83b666d
jollywatt <jo.alex.w@gmail.com>
2022-09-06 20:41:15 +1200

    Add `bits.jl` and tests

---
+++ src/bits.jl
@@ -0,0 +1,246 @@
+"""
+Operations on bits representing “unit blades”. E.g., the
+2-blade of unit norm `e₂∧e₃ ≡ e₂₃` is represented by `0b110`.
+"""
+
+bits_scalar() = unsigned(0)
+bits_first_of_grade(T::Type{<:Unsigned}, k) = (one(T) << k) - one(T)
+bits_first_of_grade(k) = bits_first_of_grade(UInt, k)
+bits_basis_vector(i) = unsigned(1) << (i - 1)
+
+
+"""
+	bits_to_indices(bits)
+
+Return the positions of the ones in the unsigned integer `bits`.
+
+Used to convert between representations of a unit blade.
+Inverse of [`indices_to_bits`](@ref).
+
+Examples
+===
+```jldoctest
+julia> GeometricAlgebra.bits_to_indices(0b1001101)
+4-element Vector{Int64}:
+ 1
+ 3
+ 4
+ 7
+```
+"""
+function bits_to_indices(bits)
+	indices = Int[]
+	i = 1
+	while bits > 0
+		if isone(bits & 1)
+			push!(indices, i)
+		end
+		i += 1
+		bits >>= 1
+	end
+	indices
+end
+
+"""
+	indices_to_bits(indices)
+
+Create unsigned integer with bits at the positions given in the vector `indices`.
+
+Used to convert between representations of a unit blade.
+Inverse of [`bits_to_indices`](@ref).
+
+Examples
+===
+```jldoctest
+julia> GeometricAlgebra.indices_to_bits([1, 2, 5]) |> UInt16 |> bitstring
+"0000000000010011"
+```
+
+"""
+function indices_to_bits(indices)
+	bits = bits_scalar()
+	for i ∈ indices
+		bits += bits_basis_vector(i)
+	end
+	bits
+end
+
+
+"""
+Return the smallest uint larger than the one given which has
+the same number of binary ones.
+Algorithm is [Gosper's hack](http://graphics.stanford.edu/~seander/bithacks.html#NextBitPermutation).
+
+```
+julia> GeometricAlgebra.next_bit_permutation(0b1011) |> bitstring
+"00001101"
+```
+"""
+function next_bit_permutation(v::Unsigned)
+	I = one(v)
+	t = v | (v - I)
+	(t + I) | (((~t & -~t) - I) >> (trailing_zeros(v) + I))
+end
+
+
+
+"""
+	BitPermutations{T}(n)
+
+Infinite iterator returning all unsigned integers of type `T`,
+in ascending order, for which `Base.count_ones` is `n`.
+"""
+struct BitPermutations{T<:Unsigned}
+	n::T
+end
+BitPermutations(n) = BitPermutations{UInt}(n)
+
+function Base.iterate(fgb::BitPermutations{T}, bits=bits_first_of_grade(T, fgb.n)) where T
+	(bits, next_bit_permutation(bits))
+end
+
+Base.IteratorSize(::BitPermutations) = Base.IsInfinite()
+
+
+
+"""
+	bits_of_grade(k[, n])
+
+Generate basis blade bits of grade `k` in ascending order.
+Yields all basis blades in the dimension `n`, if given, otherwise iterate indefinitely.
+
+Example
+===
+```jldoctest
+julia> GeometricAlgebra.bits_of_grade(2, 4) .|> UInt8 .|> bitstring
+6-element Vector{String}:
+ "00000011"
+ "00000101"
+ "00000110"
+ "00001001"
+ "00001010"
+ "00001100"
+```
+"""
+bits_of_grade(k) = BitPermutations(k)
+bits_of_grade(k, n) = Iterators.take(BitPermutations(k), binomial(n, k))
+
+
+"""
+	bits_to_mv_index(bits::Unsigned)
+
+Convert a unit blade `bits` to a linear index for accessing components of a `Multivector`. 
+"""
+function bits_to_mv_index(bits::Unsigned)
+	# From combinatorial number systems, an explicit formula is:
+	# sum(binomial(i, c[i]) for i in 1:length(c)) where c = bits_to_indices(bits)
+	ith = 1
+	c = 0
+	k = 1
+	while bits > 0
+		if isone(bits & 1)
+			ith += binomial(c, k)
+			k += 1
+		end
+		bits >>= 1
+		c += 1
+	end
+	ith
+end
+
+"""
+	mv_index_to_bits(ith, k)
+
+Convert a linear index of a component of a `k`-grade `Multivector` into the
+corresponding unit blade.
+"""
+function mv_index_to_bits(ith, k)
+	bits = bits_scalar()
+	for b ∈ Iterators.take(bits_of_grade(k), ith)
+		bits = b
+	end
+	bits
+end
+
+
+function multivector_index_offset(k, dim)
+	ith = 0
+	for i in 0:k - 1
+		ith += binomial(dim, i)
+	end
+	ith
+end
+
+"""
+	bits_to_mmv_index(bits::Unsigned)
+
+Convert a unit blade `bits` to a linear index for accessing components of a `MixedMultivector`. 
+"""
+function bits_to_mmv_index(bits, dim)
+	multivector_index_offset(count_ones(bits), dim) + bits_to_mv_index(bits)
+end
+
+
+const MULTIVECTOR_INDICES = Dict{Int,Vector{UInt}}()
+
+"""
+	mmv_index_to_bits(ith, dim)
+
+Convert a linear index of a component of a `MixedMultivector` of dimension `dim`
+into the corresponding unit blade. Uses a lookup table `$(@__MODULE__).MULTIVECTOR_INDICES` for speed.
+"""
+function mmv_index_to_bits(ith, dim)
+	if !(dim in keys(MULTIVECTOR_INDICES))
+		bits = unsigned.(0:2^dim - 1)
+		MULTIVECTOR_INDICES[dim] = bits[sortperm(bits_to_mmv_index.(bits, dim))]
+	end
+	MULTIVECTOR_INDICES[dim][ith]
+end
+
+
+"""
+Compute sign flips of blade product due to transposing basis vectors.
+(The full sign of the product will also depend on the basis norms.)
+"""
+function sign_from_swaps(a::Unsigned, b::Unsigned)
+	swaps = 0
+	while b > 0
+		a >>= 1
+		swaps += count_ones(a)*(b & 1)
+		b >>= 1
+	end
+	iseven(swaps) ? 1 : -1
+end
+
+
+"""
+	factor_from_squares(sig, bits::Unsigned)
+
+Compute the overall factor arising from the geometric product between
+repeated basis vectors
+"""
+function factor_from_squares(sig, bits::Unsigned)
+	factor = 1
+	bv = 1
+	while bits > 0
+		if isone(bits & 1)
+			factor *= sig[bv]
+		end
+		bits >>= 1
+		bv += 1
+	end
+	factor
+end
+
+
+"""
+	geometric_prod_bits(sig, a::Unsigned, b::Unsigned)
+
+Compute the geometric product between unit blades. Returns a tuple
+of the overall scalar factor and the resulting unit blade.
+"""
+function geometric_prod_bits(sig, a::Unsigned, b::Unsigned)
+	factor = sign_from_swaps(a, b)*factor_from_squares(sig, a & b)
+	bits = a ⊻ b
+	factor, bits
+end
--- test/bits.jl
+++ test/bits.jl
@@ -1,1 +1,80 @@
-@test 1 + 1 == 2
+using Multivectors:
+	bits_to_indices,
+	indices_to_bits,
+	BitPermutations,
+	mv_index_to_bits,
+	bits_to_mv_index,
+	bits_to_mmv_index,
+	mmv_index_to_bits,
+	sign_from_swaps,
+	factor_from_squares,
+	geometric_prod_bits
+
+
+@testset "bits <-> indices" begin
+	for bits ∈ [0b000, 0b101, 0b110, 0b1101001, rand(UInt16), rand(UInt64)]
+		@test indices_to_bits(bits_to_indices(bits)) == bits
+	end
+end
+
+@testset "bit permutations" begin
+	@test first(BitPermutations{UInt8}(3)) === UInt8(0b111)
+	@test first(BitPermutations{UInt32}(3)) === UInt32(0b111)
+	@test collect(Iterators.take(BitPermutations(2), 3)) == [0b011, 0b101, 0b110]
+	@test collect(Iterators.take(BitPermutations(5), 3)) == [0b011111, 0b101111, 0b110111]
+end
+
+@testset "bits <-> multivector index" begin
+	cases = [
+		(1, 0) => 0b000,
+		(1, 1) => 0b001,
+		(2, 1) => 0b010,
+		(1, 2) => 0b011,
+		(3, 1) => 0b100,
+		(2, 2) => 0b101,
+		(3, 2) => 0b110,
+		(1, 3) => 0b111,
+		(1, 8) => 0b01111_1111,
+		(2, 8) => 0b10111_1111,
+	]
+	@testset "no. $(i) of grade $k: $bits" for ((i, k), bits) ∈ cases
+		@test mv_index_to_bits(i, k) == bits
+		@test bits_to_mv_index(bits) == i
+	end
+
+	for _ ∈ 1:10, bits ∈ (rand(UInt8), rand(UInt16))
+		# warning: bits_to_mv_index() is typically slow for UInt32 or higher
+		@test mv_index_to_bits(bits_to_mv_index(bits), count_ones(bits)) == bits
+	end
+end
+
+@testset "bits <-> mixed multivector index" begin
+	for dim ∈ 1:2:8, i ∈ 1:2:2^dim
+		@test bits_to_mmv_index(mmv_index_to_bits(i, dim), dim) == i
+	end
+end
+
+@testset "sign_from_swaps()" begin
+	# note that typographically bits are written 'backwards'; v124 == 0b1011
+	@test sign_from_swaps(0b01, 0b10) == +1
+	@test sign_from_swaps(0b10, 0b01) == -1
+	@test sign_from_swaps(0b11, 0b01) == -1
+	@test sign_from_swaps(0b111, 0b01) == +1
+end
+
+@testset "factor_from_squares()" begin
+	@test factor_from_squares((+1, +1, +1), 0b000) == +1
+	@test factor_from_squares((+1, +1, +1), 0b111) == +1
+	@test factor_from_squares((-1, +1, +1), 0b001) == -1
+	@test factor_from_squares((-1, +1, +1), 0b010) == +1
+	@test factor_from_squares((-1, +1, +1), 0b111) == -1
+	@test factor_from_squares((-1, -1, -1), 0b101) == +1
+	@test factor_from_squares((-1, -1, -1), 0b111) == -1
+end
+
+@testset "geometric_prod_bits()" begin
+	@test geometric_prod_bits((1, 1, 1), 0b001, 0b010) == (+1, 0b011)
+	@test geometric_prod_bits((1, 1, 1), 0b010, 0b001) == (-1, 0b011)
+	@test geometric_prod_bits((1, 1, 1), 0b011, 0b001) == (-1, 0b010)
+	@test geometric_prod_bits((0, 1, 1), 0b001, 0b101) == (0, 0b100)
+end
